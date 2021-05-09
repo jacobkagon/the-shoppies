a742ff34b272fded919177730b1fdb36489a0baa
JakesProjects1 <jacob@kagon.net>
2021-05-09 14:22:29 -0400

    update button styling

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,29 +1,33 @@
-import React from 'react';
-import "./Results.module.css"
+import React from "react";
+import styles from "./Results.module.css";
 
-const Results = ({search, results, setNominations, nominations}) => {
-    return (
+const Results = ({ search, results, setNominations, nominations }) => {
+  return (
+    <div>
+      <h4>Results for "{search}"</h4>
+      {search ? (
         <div>
-        <h4>Results for "{search}"</h4>
-        { search ?
-        <div>
-        
-           <ul>
-           {results[0]?.map(movie => ( 
-              <li>{movie.Title} ({movie.Year})
-              { nominations.includes(movie) || nominations.length === 5?<button className="button-disabled">Nominate</button>
-              : <button onClick={() => setNominations([...nominations, movie])}>Nominate</button>
-              }
-             
+          <ul>
+            {results[0]?.map((movie) => (
+              <li>
+                {movie.Title} ({movie.Year})
+                {nominations.includes(movie) || nominations.length === 5 ? (
+                  <button className={styles.disabled}>Nominate</button>
+                ) : (
+                  <button
+                    
+                    onClick={() => setNominations([...nominations, movie])}
+                  >
+                    Nominate
+                  </button>
+                )}
               </li>
-           ))}
-           
-           </ul> 
-           </div>
-           : null
-        }
+            ))}
+          </ul>
         </div>
-    );
-}
+      ) : null}
+    </div>
+  );
+};
 
 export default Results;
